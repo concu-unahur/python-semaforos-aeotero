93d14abeab578456b04f235a9897f712d4562776
agustin <popal9193@gmail.com>
2020-02-13 20:29:29 -0300

    parece que anda y parece que esta sinconizado

--- comensales.py
+++ comensales.py
@@ -4,8 +4,8 @@ import logging
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
-semCli = threading.Semaphore(3)
-semCos = threading.Semaphore(0)
+semClientes = threading.Semaphore(1)
+semCocinero = threading.Semaphore(0)
 
 
 class Cocinero(threading.Thread):
@@ -16,16 +16,14 @@ class Cocinero(threading.Thread):
   def run(self):
     global platosDisponibles
     while (True):
-      semCos.acquire()
-      logging.info('Reponiendo los platos...')
-      platosDisponibles = 3
-      semCli.release()
-      semCli.release()
-      semCli.release() # como hago para hacer los 3 release sin que el tread de cliente siga despues del primero?
-      #semCli = threading.Semaphore(3)
+      semCocinero.acquire()
+      if (platosDisponibles == 0):
+        logging.info('Reponiendo los platos...')
+        platosDisponibles = 3
+      semClientes.release()
 
 Cocinero1 = Cocinero(1) # creo los 2 cocineros
-Cocinero2 = Cocinero(2)
+#Cocinero2 = Cocinero(2)
 
 class Comensal(threading.Thread):
   def __init__(self, numero):
@@ -41,12 +39,11 @@ class Comensal(threading.Thread):
 platosDisponibles = 3
 
 Cocinero1.start() #arranco los cocineros
-Cocinero2.start() # en realidad , se van alternando los cocineros 
+#Cocinero2.start() # en realidad , se van alternando los cocineros 
 
-for i in range(20):
-  semCli.acquire()
+for i in range(55):
+  semClientes.acquire()
   Comensal(i).start()
-  if (platosDisponibles == 0): #solucion a todos los problemas , un IF!!!
-    semCos.release()
+  semCocinero.release()
 
 
