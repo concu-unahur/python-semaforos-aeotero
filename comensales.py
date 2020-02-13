b27941a4ce7fa48f8f0a607a69c3cfff2b116a58
agustin <popal9193@gmail.com>
2020-02-13 19:09:40 -0300

    comenzals resuelto la primer parte con un in en el for del cliente, terminada variante de 2 cocineros

--- comensales.py
+++ comensales.py
@@ -9,18 +9,23 @@ semCos = threading.Semaphore(0)
 
 
 class Cocinero(threading.Thread):
-  def __init__(self):
+  def __init__(self, numero):
     super().__init__()
-    self.name = 'Cocinero'
+    self.name = (f'Cocinero {numero}')
 
   def run(self):
     global platosDisponibles
     while (True):
+      semCos.acquire()
       logging.info('Reponiendo los platos...')
       platosDisponibles = 3
       semCli.release()
       semCli.release()
-      semCli.release()
+      semCli.release() # como hago para hacer los 3 release sin que el tread de cliente siga despues del primero?
+      #semCli = threading.Semaphore(3)
+
+Cocinero1 = Cocinero(1) # creo los 2 cocineros
+Cocinero2 = Cocinero(2)
 
 class Comensal(threading.Thread):
   def __init__(self, numero):
@@ -29,15 +34,19 @@ class Comensal(threading.Thread):
 
   def run(self):
     global platosDisponibles
-    semCli.acquire()
     platosDisponibles -= 1
-    logging.info(f'¡Que rico! Quedan {platosDisponibles} platos')
+    logging.info(f'¡que rico! Quedan {platosDisponibles} platos')
 
 
 platosDisponibles = 3
 
-Comensal().start()
+Cocinero1.start() #arranco los cocineros
+Cocinero2.start() # en realidad , se van alternando los cocineros 
 
-for i in range(5):
+for i in range(20):
+  semCli.acquire()
   Comensal(i).start()
+  if (platosDisponibles == 0): #solucion a todos los problemas , un IF!!!
+    semCos.release()
+
 
